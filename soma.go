969871c1dfaeb05c20aaed10a73e467a9a351e1c
Gabriel Rozendo <79455923+rozendox@users.noreply.github.com>
2023-01-09 17:58:27 -0300

    Create soma.go

---
+++ soma.go
@@ -0,0 +1,13 @@
+// Sintaxe básica do go
+// Operações aritiméticas
+package main
+
+import "fmt"
+
+func main() {
+	// duas variaveis do tipo inteiro
+	var a = 20
+	var b = 7
+
+	fmt.Println("a + b =", a+b)
+}

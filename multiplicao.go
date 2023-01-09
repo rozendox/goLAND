55184b7ddf648b7220d986b9fcf31874f618556c
Gabriel Rozendo <79455923+rozendox@users.noreply.github.com>
2023-01-09 18:03:28 -0300

    Create multiplicao.go

---
+++ multiplicao.go
@@ -0,0 +1,19 @@
+// Sintaxe básica do go
+// Operações aritiméticas
+package main
+
+import "fmt"
+
+func main() {
+	// duas variaveis do tipo inteiro
+	var a = 11
+	var b = 11
+
+	fmt.Println("a * b =", a*b)
+
+	// duas variáveis do tipo float
+	var y = 12.5
+	var z = 13.9
+
+	fmt.Println("y * z =", y*z)
+}
